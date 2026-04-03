83bb015758fc6c18d7573f559f821396c32b17b4
fouad fawzy <fouadfawzy47@gmail.com>
2026-04-03 02:32:06 +0200

    init project

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF5D3FD3);
+  static const whiteColor = Color(0xFFFFFFFF);
+  static const blackColor = Color(0xFF000000);
+  static const lightGrayColor = Color(0xFFF3F0F0);
+  static const grayColor = Color(0xFFD9D9D9);
+  static const redColor = Color(0xFFE53953);
+  static const blueColor = Color(0xFF1C83E9);
+  static const darkGrayColor = Color(0xFFB7B3B7);
+}
