90b27386370dba090dfc64d6feb03e25204adea0
fouad fawzy <fouadfawzy47@gmail.com>
2026-04-05 10:03:34 +0200

    edit on login page

--- lib/features/ui/auth/login/login_screen.dart
+++ lib/features/ui/auth/login/login_screen.dart
@@ -17,64 +17,80 @@ class LoginScreen extends StatelessWidget {
 
   final TextEditingController passwordController = TextEditingController();
 
-  GlobalKey<FormState> formKey = GlobalKey();
+  final GlobalKey<FormState> formKey = GlobalKey();
 
   bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        toolbarHeight: 288.h,
-        backgroundColor: AppColors.primaryColor,
-        title: Column(
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            Image.asset(AppAssets.userImage),
-            Text('Sign in', style: AppStyles.regural32White),
-          ],
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 17.w, vertical: 32.h),
-        child: SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                CustomTextFormField(
-                  hintText: 'email address',
-                  labelText: 'Email',
-                  icon: Icons.email,
-                  controller: emailController,
-                  validator: (value) {
-                    return AppValidators.validateEmail(email: value);
-                  },
+            Container(
+              height: 320.h,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(60.r),
+                  bottomRight: Radius.circular(60.r),
                 ),
-                SizedBox(height: 16.h),
-                CustomPasswordTextField(
-                  hintText: 'password',
-                  controller: passwordController,
-                  validator: (value) {
-                    return AppValidators.validatePassword(password: value);
-                  },
-                ),
-                SizedBox(height: 72.h),
-                CustomElevatedButton(
-                  text: 'Log in',
-                  isLoading: isLoading,
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      Navigator.pushReplacementNamed(
-                        context,
-                        AppRoutes.studentRoute,
-                      );
-                    }
-                  },
-                ),
-              ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 40.h),
+                  Image.asset(AppAssets.userImage, height: 120.h),
+                  SizedBox(height: 8.h),
+                  Text('Sign in', style: AppStyles.regural32White),
+                ],
+              ),
             ),
-          ),
+
+            // Form Section
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 40.h),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CustomTextFormField(
+                      hintText: 'email address',
+                      labelText: 'Email',
+                      icon: Icons.email_outlined,
+                      controller: emailController,
+                      validator: (value) =>
+                          AppValidators.validateEmail(email: value),
+                    ),
+                    SizedBox(height: 20.h),
+                    CustomPasswordTextField(
+                      hintText: 'password',
+                      controller: passwordController,
+                      validator: (value) =>
+                          AppValidators.validatePassword(password: value),
+                    ),
+
+                    SizedBox(height: 60.h),
+
+                    CustomElevatedButton(
+                      text: 'Log in',
+                      isLoading: isLoading,
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          Navigator.pushReplacementNamed(
+                            context,
+                            AppRoutes.studentRoute,
+                          );
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
